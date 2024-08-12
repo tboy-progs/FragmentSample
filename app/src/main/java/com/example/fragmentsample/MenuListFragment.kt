b6c4f5541283dac7d3c85d4c3f2e886b68e168a1
taka <gluestick829@gmail.com>
2024-08-12 20:13:14 +0900

    Add:メニューリストタップ時の処理を追加

--- app/src/main/java/com/example/fragmentsample/MenuListFragment.kt
+++ app/src/main/java/com/example/fragmentsample/MenuListFragment.kt
@@ -1,7 +1,9 @@
 package com.example.fragmentsample
 
+import MenuThanksFragment
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.SimpleAdapter
 import androidx.fragment.app.Fragment
@@ -25,5 +27,23 @@ class MenuListFragment : Fragment(R.layout.fragment_menu_list) {
             SimpleAdapter(activity, menuList, android.R.layout.simple_list_item_2, from, to)
 
         lvMenu.adapter = adapter
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            val item = parent.getItemAtPosition(position) as MutableMap<String, String>
+            val menuName = item["name"]
+            val menuPrice = item["price"]
+
+            val bundle = Bundle()
+            bundle.putString("menuName", menuName)
+            bundle.putString("menuPrice", menuPrice)
+
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.setReorderingAllowed(true)
+            transaction.addToBackStack("Only List")
+            transaction.replace(R.id.fragmentMainContainer, MenuThanksFragment::class.java, bundle)
+            transaction.commit()
+        }
     }
 }
