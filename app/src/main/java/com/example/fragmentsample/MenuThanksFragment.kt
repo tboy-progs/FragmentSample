f5e792289205a8e5caf01a78f093977134bafcdb
taka <gluestick829@gmail.com>
2024-08-12 20:07:23 +0900

    Add:注文完了画面の表示処理を追加

--- app/src/main/java/com/example/fragmentsample/MenuThanksFragment.kt
+++ app/src/main/java/com/example/fragmentsample/MenuThanksFragment.kt
@@ -1,59 +1,30 @@
-package com.example.fragmentsample
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.fragmentsample.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class MenuThanksFragment : Fragment(R.layout.fragment_menu_thanks) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MenuThanksFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MenuThanksFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+        val menuName = arguments?.getString("menuName") ?: ""
+        val menuPrice = arguments?.getString("menuPrice") ?: ""
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        val tvMenuName = view.findViewById<TextView>(R.id.tvMenuName)
+        val tvMenuPrice = view.findViewById<TextView>(R.id.tvMenuPrice)
+
+        tvMenuName.text = menuName
+        tvMenuPrice.text = menuPrice
+
+        val btBackButton = view.findViewById<Button>(R.id.btThxBack)
+        btBackButton.setOnClickListener(ButtonClickListener())
+    }
+
+    private inner class ButtonClickListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+            parentFragmentManager.popBackStack()
         }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_thanks, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuThanksFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MenuThanksFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
